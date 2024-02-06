22a2152fdfff9d073e798c626dcc1893147da742
Renan Meneses de Andrade Franca <renan.andradefranca@gmail.com>
2024-02-06 09:17:46 -0300

    Apply gradle-java module

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+  java
+  jacoco
+  // jhipster-needle-gradle-plugins
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+jacoco {
+  toolVersion = libs.versions.jacoco.get()
+}
+
+// jhipster-needle-gradle-plugins-configurations
+
+repositories {
+  mavenCentral()
+  // jhipster-needle-gradle-repositories
+}
+
+group = "com.mycompany.myapp"
+version = "0.0.1-SNAPSHOT"
+
+ext {
+  // jhipster-needle-gradle-properties
+}
+
+dependencies {
+  // jhipster-needle-gradle-dependencies
+  testImplementation(libs.junit.engine)
+  testImplementation(libs.junit.params)
+  testImplementation(libs.assertj)
+  testImplementation(libs.mockito)
+  // jhipster-needle-gradle-test-dependencies
+}
+
+tasks.test {
+  filter {
+    includeTestsMatching("*Test.*")
+    excludeTestsMatching("*IT.*")
+  }
+  useJUnitPlatform()
+}
+
+val integrationTest = task<Test>("integrationTest") {
+  description = "Runs integration tests."
+  group = "verification"
+  shouldRunAfter("test")
+  filter {
+    includeTestsMatching("*IT.*")
+    excludeTestsMatching("*Test.*")
+  }
+  useJUnitPlatform()
+}
